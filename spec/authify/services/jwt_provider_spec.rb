a640e815ac98d3db49dfa6b2096fe3d67da69ea1
Jonathan Gnagy <jgnagy@knuedge.com>
2017-03-15 14:19:25 -0700

    Finishing tests for JWT endpoints

--- spec/authify/services/jwt_provider_spec.rb
+++ spec/authify/services/jwt_provider_spec.rb
@@ -50,13 +50,43 @@ describe Authify::API::Services::JWTProvider do
 
   # /token
   context 'token provider endpoint' do
+    let(:incorrect_credentials) do
+      {
+        'email' => RSpec.configuration.test_user.email,
+        'password' => 'foobar123'
+      }.to_json
+    end
+
+    let(:api_auth_data) do
+      {
+        'access-key' => RSpec.configuration.test_user_apikey.access_key,
+        'secret-key' => RSpec.configuration.test_user_apikey.secret_key
+      }.to_json
+    end
+
+    let(:user_auth_data) do
+      {
+        'email' => RSpec.configuration.test_user.email,
+        'password' => 'testuser123'
+      }.to_json
+    end
+
+    let(:trusted_delegate_auth_data) do
+      {
+        'delegate' => {
+          'access' => RSpec.configuration.trusted_delegate.access_key,
+          'secret' => RSpec.configuration.trusted_delegate.secret_key
+        },
+        'provider' => RSpec.configuration.test_user_identity.provider,
+        'uid' => RSpec.configuration.test_user_identity.uid
+      }.to_json
+    end
+
     context 'with incorrect credentials' do
       it 'returns 401' do
         header 'Accept', 'application/json'
-        post '/token', {
-          'email' => RSpec.configuration.test_user.email,
-          'password' => 'foobar123'
-        }.to_json
+        header 'Content-Type', 'application/json'
+        post '/token', incorrect_credentials
 
         # Should respond with a 401
         expect(last_response.status).to eq(401)
@@ -66,10 +96,40 @@ describe Authify::API::Services::JWTProvider do
     context 'using an email and password' do
       it 'provides a usable JWT' do
         header 'Accept', 'application/json'
-        post '/token', {
-          'email' => RSpec.configuration.test_user.email,
-          'password' => 'testuser123'
-        }.to_json
+        header 'Content-Type', 'application/json'
+        post '/token', user_auth_data
+
+        # Should respond with a 200
+        expect(last_response.status).to eq(200)
+
+        details = JSON.parse(last_response.body)
+
+        expect(details.size).to eq(1)
+        expect(details).to have_key('jwt')
+      end
+    end
+
+    context 'using an API access key and secret' do
+      it 'provides a usable JWT' do
+        header 'Accept', 'application/json'
+        header 'Content-Type', 'application/json'
+        post '/token', api_auth_data
+
+        # Should respond with a 200
+        expect(last_response.status).to eq(200)
+
+        details = JSON.parse(last_response.body)
+
+        expect(details.size).to eq(1)
+        expect(details).to have_key('jwt')
+      end
+    end
+
+    context 'via a trusted delegate and identity' do
+      it 'provides a usable JWT' do
+        header 'Accept', 'application/json'
+        header 'Content-Type', 'application/json'
+        post '/token', trusted_delegate_auth_data
 
         # Should respond with a 200
         expect(last_response.status).to eq(200)
