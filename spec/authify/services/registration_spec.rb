970f618442b3f48c08f250ae536dab6fe98951a7
Jonathan Gnagy <jgnagy@knuedge.com>
2017-04-21 17:39:37 -0700

    Adding the ability to reset forgotten passwords

--- spec/authify/services/registration_spec.rb
+++ spec/authify/services/registration_spec.rb
@@ -67,5 +67,82 @@ describe Authify::API::Services::Registration do
         end
       end
     end
+
+    context 'forgot_password endpoint' do
+      let(:forgot_password_data) do
+        {
+          'email' => 'third.user@example.com'
+        }
+      end
+
+      let(:password_reset_data) do
+        u = Authify::API::Models::User.find_by_email('third.user@example.com')
+        full_token = u.verification_token
+        {
+          'email'    => 'third.user@example.com',
+          'password' => 'thirduser1234',
+          'token'    => full_token.split(':').first
+        }
+      end
+
+      let(:invalid_data) do
+        {
+          'email' => 'nonexistent@example.com'
+        }
+      end
+
+      context 'POST /forgot_password' do
+        it 'sets a verification token' do
+          header 'Accept', 'application/json'
+          header 'Content-Type', 'application/json'
+          expect(
+            Authify::API::Models::User.find_by_email(
+              forgot_password_data['email']
+            ).verification_token
+          ).to eq(nil)
+          post '/forgot_password', forgot_password_data.to_json
+
+          # Should respond with a 200
+          expect(last_response.status).to eq(200)
+          expect(last_response.body).to eq('')
+          expect(
+            Authify::API::Models::User.find_by_email(
+              forgot_password_data['email']
+            ).verification_token
+          ).not_to eq(nil)
+        end
+
+        it 'allows resetting a password' do
+          header 'Accept', 'application/json'
+          header 'Content-Type', 'application/json'
+          post '/forgot_password', password_reset_data.to_json
+
+          # Should respond with a 200
+          expect(last_response.status).to eq(200)
+
+          details = JSON.parse(last_response.body)
+          expect(details.size).to eq(4)
+          expect(details).to have_key('verified')
+          expect(details['verified']).to be(true)
+          expect(details).to have_key('jwt')
+          expect(details['email']).to eq(password_reset_data['email'])
+          expect(
+            Authify::API::Models::User.find_by_email('third.user@example.com').authenticate(
+              password_reset_data['password']
+            )
+          ).to eq(true)
+        end
+
+        it 'can not be used to find users' do
+          header 'Accept', 'application/json'
+          header 'Content-Type', 'application/json'
+          post '/forgot_password', invalid_data.to_json
+
+          # Should respond with a 200
+          expect(last_response.status).to eq(200)
+          expect(last_response.body).to eq('')
+        end
+      end
+    end
   end
 end
