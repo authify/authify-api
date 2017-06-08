4ce1185e51c6ebd199540e938b8eeb5bae9b1c22
Jonathan Gnagy <jgnagy@knuedge.com>
2017-06-08 14:26:18 -0700

    Adding tests for /organizations/mine

--- spec/authify/services/api/organizations_spec.rb
+++ spec/authify/services/api/organizations_spec.rb
@@ -51,7 +51,7 @@ describe Authify::API::Services::API do
         expect(error['detail']).to eq('You are not authorized to perform this action')
       end
 
-      it 'allows a user to list organizations' do
+      it 'allows a user to list all organizations' do
         header 'Accept', 'application/vnd.api+json'
         header 'Authorization', "Bearer #{user_jwt}"
         get '/organizations'
@@ -192,6 +192,46 @@ describe Authify::API::Services::API do
 
         # Should respond with a 204
         expect(last_response.status).to eq(204)
+      end
+    end
+
+    context 'GET /organizations/mine' do
+      it 'requires authentication' do
+        header 'Accept', 'application/vnd.api+json'
+        get '/organizations/mine'
+
+        # Should respond with a 403
+        expect(last_response.status).to eq(403)
+
+        error_details = JSON.parse(last_response.body)['errors']
+        error = error_details.last
+
+        # Should only be 1 error
+        expect(error_details.size).to eq(1)
+        expect(error['title']).to eq('Forbidden Error')
+      end
+
+      it 'allows a user to list their own organizations' do
+        header 'Accept', 'application/vnd.api+json'
+        header 'Authorization', "Bearer #{user_jwt}"
+        get '/organizations/mine'
+
+        # Should respond with a 200
+        expect(last_response.status).to eq(200)
+
+        details = JSON.parse(last_response.body)
+
+        expect(details.size).to eq(3)
+        expect(details).to have_key('data')
+        expect(details['data'].size).to eq(1)
+        expect(details['data'].first).to have_key('id')
+        expect(details['data'].first['id']).to eq('1')
+        expect(details['data'].first).to have_key('attributes')
+        expect(details['data'].first['attributes']).to have_key('name')
+        expect(details['data'].first['attributes']['name']).to eq('test')
+        expect(details['data'].first['attributes']).to have_key('description')
+        expect(details).to have_key('jsonapi')
+        expect(details['jsonapi']).to eq('version' => '1.0')
       end
     end
   end
